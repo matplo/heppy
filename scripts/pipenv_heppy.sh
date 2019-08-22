d8965bcf7cc96737a66ba036d4baaea7e656a4cb
matplo <ploskon@gmail.com>
2019-08-22 17:09:58 -0400

    pipenv assist - point to in-heppy venv directory

---
+++ scripts/pipenv_heppy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+savedir=${PWD}
+
+function thisdir()
+{
+	SOURCE="${BASH_SOURCE[0]}"
+	while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+	  SOURCE="$(readlink "$SOURCE")"
+	  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	done
+	DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+	echo ${DIR}
+}
+THISD=$(thisdir)
+source ${THISD}/util.sh
+separator "${BASH_SOURCE}"
+
+cd ${THISD}/..
+
+export WORKON_HOME=$PWD/venv
+pipenv $@
+
+cd ${savedir}
