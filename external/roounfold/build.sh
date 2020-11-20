7b23e23226db003ece939dcf4e1699426d3d5e7d
matplo <ploskon@gmail.com>
2020-11-20 01:19:38 -0800

    RooUnfold in heppy - build for version 2.0.0

---
+++ external/roounfold/build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+cdir=$(pwd)
+
+function thisdir()
+{
+	SOURCE="${BASH_SOURCE[0]}"
+	while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+	  SOURCE="$(readlink "$SOURCE")"
+	  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	done
+	DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+	echo ${DIR}
+}
+THISD=$(thisdir)
+cd ${THISD}
+
+. ${THISD}/../../scripts/util.sh
+
+separator "building roounfold ${PWD}"
+
+clean=$(get_opt "clean" $@)
+if [ ! -z ${clean} ]; then
+	separator "clean"
+	rm -rf ./build_roounfold
+fi
+
+cleanall=$(get_opt "cleanall" $@)
+if [ ! -z ${cleanall} ]; then
+	separator "cleanall"
+	rm -rf ./build_roounfold ./roounfold-2.0.0
+fi
+
+mkdir -p ./build_roounfold
+cd ./build_roounfold
+
+roounfold_version=2.0.0
+roounfold_heppy_prefix="${THISD}/roounfold-${roounfold_version}"
+separator configuration
+cmake -DCMAKE_BUILD_TYPE=Release -DROOUNFOLD_VERSION="${roounfold_version}" -DROOUNFOLD_HEPPY_PREFIX=${roounfold_heppy_prefix} ..
+separator build
+cmake --build . --target all  
+
+cd ${cdir}
+
+if [ -e ${roounfold_heppy_prefix}/bin/RooUnfoldTest ]; 
+then
+	ln -sf ${roounfold_heppy_prefix} ${THISD}/roounfold-current
+	separator summary
+	ls -l ${roounfold_heppy_prefix}/include
+	ls -l ${roounfold_heppy_prefix}/lib
+	ls -l ${roounfold_heppy_prefix}/bin
+else
+	echo_error "[e] sorry... the build failed: no RooUnfoldTest in ${roounfold_heppy_prefix}/bin/"
+	separator "roounfold build script done"
+	exit 1
+fi
+separator "roounfold build script done"
