80db72b5f4bbbbf460f393616290438af78735b8
Ezra Lesser <ezralesser@gmail.com>
2022-03-09 06:57:57 -0800

    Formatting fix

--- cpptools/src/fjext/fjtools.cxx
+++ cpptools/src/fjext/fjtools.cxx
@@ -149,7 +149,7 @@ namespace FJTools
 
 
 	//-----------------------------------------------------------------------------------
-	/// Custom recombiner class 
+	/// Custom recombiner class
 	/// Allows to recombine negative user_index particles by subtracting rather than adding their four-vectors
 	/// Code adapted from: Yasuki Tachibana
 	NegativeEnergyRecombiner::NegativeEnergyRecombiner():
@@ -158,38 +158,36 @@ namespace FJTools
 	NegativeEnergyRecombiner::NegativeEnergyRecombiner(const int ui):
 		_ui(ui)
 	{;}
-				
-	void NegativeEnergyRecombiner::recombine(const fastjet::PseudoJet & particle1,
-													 const fastjet::PseudoJet & particle2,
-													 fastjet::PseudoJet & combined_particle) const {
-		
+
+	void NegativeEnergyRecombiner::recombine(
+		const fastjet::PseudoJet & particle1, const fastjet::PseudoJet & particle2,
+		fastjet::PseudoJet & combined_particle) const {
+
 		// Define coefficients with which to combine particles
 		// If particle has positive user_index: +1 (i.e. add its four-vector)
 		// If particle has negative user_index: -1 (i.e. subtract its four-vector)
 		int c1=1,c2=1;
-		if (particle1.user_index() < 0) { 
+		if (particle1.user_index() < 0) {
 			c1 = -1;
 		}
-		if (particle2.user_index() < 0) { 
+		if (particle2.user_index() < 0) {
 			c2 = -1;
 		}
-		
+
 		// Recombine particles
 		combined_particle = c1*particle1 + c2*particle2;
-		
-		// If the combined particle has negative energy, flip the four-vector 
+
+		// If the combined particle has negative energy, flip the four-vector
 		// and assign it a new user index
 		if(combined_particle.E() < 0) {
 			combined_particle.set_user_index(_ui);
-			combined_particle.reset_momentum(-combined_particle.px(),
-												-combined_particle.py(),
-												-combined_particle.pz(),
-												-combined_particle.E());
-		} else { 
+			combined_particle.reset_momentum(
+				-combined_particle.px(), -combined_particle.py(),
+				-combined_particle.pz(), -combined_particle.E());
+		} else {
 			combined_particle.set_user_index(0);
 		}
 	}
-
 
 	//-----------------------------------------------------------------------------------
 
